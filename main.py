39e5889cd22892383b0bda3ddcf3c7d402d85bb1
Leon <limiroshnichenko@miem.hse.ru>
2022-08-18 19:10:00 +0300

    message style changes

--- main.py
+++ main.py
@@ -32,18 +32,18 @@ def webhook_endpoint(stream_name: str, topic_name: str, data: TaigaWebhook):
             return
         diff = data["change"]["diff"]["status"]
         text = (
-            f"**Project:** `{project_name}`\n"
-            f"**Userstory:** `{us_name}`\n"
-            f"**Task:** `{task_name}`\n\n"
+            f"**Project:** {project_name}\n"
+            f"**Userstory:** {us_name}\n"
+            f"**Task:** {task_name}\n\n"
             f"@_**{initiator_full_name}** изменил статус задачи с "
             f"**{diff['from']}** на **{diff['to']}**."
         )
     elif data["change"]["comment"] != "":
         comment = data["change"]["comment"]
         text = (
-            f"**Project:** `{project_name}`\n"
-            f"**Userstory:** `{us_name}`\n"
-            f"**Task:** `{task_name}`\n\n"
+            f"**Project:** {project_name}\n"
+            f"**Userstory:** {us_name}\n"
+            f"**Task:** {task_name}\n\n"
             f"**Комментарий от** @_**{initiator_full_name}**:\n"
             f"```spoiler\n{comment}\n```"
         )
